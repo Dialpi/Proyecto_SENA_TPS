69622b8e07996a4a57a926557f0f6a5cf604d35b
Dialpi <Diegoapirazan@gmail.com>
2022-10-30 23:55:02 -0500

    creacion de caroucel y boton de tasas

--- PaginaWeb/carousel.js
+++ PaginaWeb/carousel.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', function(){
-    new Glider(document.querySelector('.carousel__lista1'), {
+    new Glider(document.querySelector('.carousel__lista'), {
         slidesToScroll: 1,
         slidesToShow: 3,
         draggable: false,
-        dots: '.carousel__indicadores1',
+        dots: '.carousel__indicadores',
         arrows: {
-          prev: '.carousel__anterior1',
-          next: '.carousel__siguiente1'
+          prev: '.carousel__anterior',
+          next: '.carousel__siguiente'
         },
         responsive: [
             {
@@ -30,14 +30,45 @@ window.addEventListener('load', function(){
 
 });
 window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel__lista1'), {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      draggable: false,
+      dots: '.carousel__indicadores1',
+      arrows: {
+        prev: '.carousel__anterior1',
+        next: '.carousel__siguiente1'
+      },
+      responsive: [
+          {
+            // si la pantalla es mas grande que  >= 500px
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            }
+          },{
+            // si la pantalla es mas grande de >= 800px
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 2,
+              
+            }
+          }
+        ]
+  })
+
+});
+window.addEventListener('load', function(){
   new Glider(document.querySelector('.carousel__lista2'), {
       slidesToScroll: 1,
       slidesToShow: 3,
-      draggable: true,
-      dots: '.carousel__indicadores',
+      draggable: false,
+      dots: '.carousel__indicadores2',
       arrows: {
-        prev: '.carousel__anterior',
-        next: '.carousel__siguiente'
+        prev: '.carousel__anterior2',
+        next: '.carousel__siguiente2'
       },
       responsive: [
           {
